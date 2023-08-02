61acf296593af1efc16b5541540f0d3f403af4fd
MatheusFreitasG <131834576+MatheusFreitasG@users.noreply.github.com>
2023-08-02 13:19:27 -0300

    Dog challenges

---
+++ dog.js
@@ -0,0 +1,54 @@
+/*
+ Exercicio 1
+ essa variavel armazena minha idade em valores humanos
+*/
+let myAge = 16;
+
+/*
+ Exercicio 2
+ vou criar uma variavel com os 2 primeiros anos de um cão
+*/
+
+let earlyYears = 2;
+
+// Exercicio 3
+
+earlyYears = earlyYears * 10.5
+console.log(`a quantidade de "anos de cão" que cada
+um dos dois primeiros anos humanos representa ${earlyYears}`)
+
+/*
+ Exercicio 4
+ subitrai a minha idade por 2
+*/
+
+let laterYears = myAge - 2
+
+/*
+ Exercicio 5
+ multipliquei a minha idade subitraida por 4
+*/
+
+laterYears = laterYears * 4
+
+// Exercicio 6
+
+console.log(earlyYears, laterYears)
+
+
+// Exercicio 7 (representa minha idade em anos de cão)
+
+let myAgeInDogYears = laterYears + earlyYears
+
+// Exercicio 8 (converte as letras maiusculas em minusculas)
+
+let myName = 'MATHEUS DE FREITAS';
+console.log(myName.toLowerCase())
+
+// Exercicio 9 (fiz uma interpolação com as informações que adiquirimos)
+
+console.log(`Meu nome é ${myName}. Tenho ${myAge} anos em anos humanos, que são ${myAgeInDogYears} anos em anos de cão`)
+
+// Exercicio 10 (Alterei)
+
+
