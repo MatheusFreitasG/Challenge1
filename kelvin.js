c4a504bf7ac2f62e7e8a8aad0529d765e5c7dcc3
MatheusFreitasG <131834576+MatheusFreitasG@users.noreply.github.com>
2023-08-02 11:25:05 -0300

    Challenge kelvin

---
+++ kelvin.js
@@ -0,0 +1,53 @@
+// Exercicio 1
+let kelvin = 293;
+
+console.log(kelvin);
+
+/*
+Exercicio 2 
+criei uma variavel de 293 kelvin
+ */
+
+// Exercicio 3
+
+let celcius = kelvin - 273;
+console.log(celcius);
+
+/*
+ Exercicio 4
+ criei uma variavel que subitrai kelvin por graus celcius e armazenei o resultado em uma variavel
+*/
+
+// Exercicio 5
+
+let farenheit = (celcius * (9/5)) + 32;
+console.log(farenheit);
+
+/*
+ Exercicio 6
+ fiz um calculo para transformar graus celcius em farenheit e armazenei o valor em uma variavel
+*/
+
+// Exercicio 7
+
+farenheit = Math.floor(farenheit);
+
+/*
+ Exercicio 8
+ atribui um arredondamento da biblioteca matematica para arrendodar o valor da variavel
+*/
+
+// Exercicio 9
+
+console.log(`A temperatura é ${farenheit} graus Fahrenheit`);
+
+// Exercicio 10 (Executei)
+
+// Exercicio 11 (a conta da -460 farenheit)
+
+// Exercicio 12
+
+newton = celcius * (33/100);
+newton = Math.floor(newton);
+console.log(newton)
+
